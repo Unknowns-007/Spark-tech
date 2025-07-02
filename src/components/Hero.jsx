e968dc94714d5e716596eb7620717bea26ea89b4
Unknowns-007 <vignesh2262006@gmail.com>
2025-07-02 19:53:19 +0530

    Update Navbar and layout; add new Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+  return (
+    <section className="text-white bg-black min-h-screen flex items-center justify-center px-4">
+      <div className="text-center">
+        <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight">
+          The Best <span className="inline-block bg-orange-500 text-black px-2 rotate-[-2deg]">Digital</span>
+        </h1>
+        <h1 className="text-5xl md:text-7xl font-bold leading-tight mt-2">Marketing <span className="text-orange-500">Agency.</span></h1>
+        <p className="mt-6 text-lg text-gray-300 max-w-2xl mx-auto">
+          We believe in combining innovative design, sustainable practices, and exceptional craftsmanship to bring your vision to life.
+        </p>
+        <button className="group bg-orange-400 hover:bg-orange-500 text-black font-semibold mt-10 px-6 py-3 rounded-full transition-all duration-300 inline-flex items-center">
+  CONTACT US
+  <span className="ml-2   transform transition-all duration-500 ease-in-out group-hover:translate-x-2 group-hover:scale-125 ">
+    →
+  </span>
+</button>
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import { useState } from 'react'
 export const Navbar = () => {
     const [active, setActive] = useState('home');
 
-  const menuItems = ['home', 'services', 'projects', 'contact', 'about', 'reviews'];
+  const menuItems = ['home', 'services', 'projects',  'about', 'reviews'];
 
   return (
-   <div className='h-[60px] w-[650px] border rounded-4xl ml-[400px] mt-[20px] flex items-center bg-black'>
+    
+       <div className='bg-black h-[80px] w-full pt-[10px]'>
+      <div className='h-[60px] w-[650px] border-[#c8c8c8] border rounded-4xl  fixed top-0 left-0 ml-[400px] mt-[20px]  flex items-center bg-black font-[Inter] text-sm md:text-base tracking-wide'>
       <ul className='flex justify-evenly items-center w-full h-full'>
         {menuItems.map((item) => (
           <li
@@ -16,7 +18,7 @@ export const Navbar = () => {
             className={`cursor-pointer px-4 py-2 rounded-full text-sm font-semibold transition-all duration-200 ${
               active === item
                 ? 'bg-[#262424] text-[#BE6A26]'
-                : 'text-white hover:text-[13px] hover:bg-[#d0d0d0]'
+                : 'text-white hover:text-[13px] hover:bg-[#4f4e4e]'
             }`}
           >
             {item.charAt(0).toUpperCase() + item.slice(1)}
@@ -24,6 +26,7 @@ export const Navbar = () => {
         ))}
       </ul>
     </div>
+    </div>
   );
 }
 export default Navbar
