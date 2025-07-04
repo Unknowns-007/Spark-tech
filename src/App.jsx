d56fc4a337aa8941a1e99b8b7e04531e2a2be2b0
Vignesh <164555796+Unknowns-007@users.noreply.github.com>
2025-07-04 20:23:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Navbar } from "./components/Navbar"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
+import FAQ from "./components/FAQ"
 
 function App() {
    return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Services/>
+       <FAQ/>
     </div>
   )
 }
