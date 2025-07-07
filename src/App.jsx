3c7f74c71e8144641c8fe8c0b229fc1ce207beb4
Iraiyanbust <iraiyanbust@gmail.com>
2025-07-07 20:34:51 +0530

    Update App.jsx(

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Navbar } from "./components/Navbar"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
 import Faq from "./components/Faq"
-import ContactForm from "./components/ContactForm"
+import Footer from "./components/Footer"
+
 
 function App() {
    return (
@@ -13,6 +14,7 @@ function App() {
       <Services/>
        <Faq/>
       <ContactForm/>
+      <Footer/>
     </div>
   )
 }
