7fde9ee430a55e811df8f274abeecf8f15a28dbe
Iraiyanbust <iraiyanbust@gmail.com>
2025-07-11 17:36:46 +0530

    added blogs and faq

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import About from './components/About';
-import Blogs from './components/Blogs';
-import ProjectsSection from './components/ProjectsSection';
-import Footer from './components/Footer';
-import ContactForm from './components/ContactForm';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import Blogs from "./components/Blogs";
+import BlogDetail from "./components/BlogDetail";
+import BlogsPage from "./components/BlogsPage";
+import ProjectsSection from "./components/ProjectsSection";
+import Footer from "./components/Footer";
+import ContactForm from "./components/ContactForm";
+import faq, { Faq } from "./components/Faq"
 
 const Home = () => (
   <>
@@ -17,24 +20,28 @@ const Home = () => (
     <div id="services">
       <Services />
     </div>
-    <div id="footer">
-      <Footer />
+    <div id="faq">
+      <Faq />
     </div>
     <div id="contact">
       <ContactForm />
+    </div>
+     <div id="footer">
+      <Footer />
     </div>
   </>
 );
 
 const App = () => {
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<ProjectsSection />} />
-        <Route path='/blogs' element={<Blogs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<ProjectsSection />} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
       </Routes>
     </div>
   );
